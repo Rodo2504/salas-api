f055cbd7d774b08504245b773a4c48e1f97c1246
unknown <rodolfoq60@gmail.com>
2022-07-12 14:54:32 -0500

    databse changes

--- src/conexion.js
+++ src/conexion.js
@@ -1,9 +1,8 @@
 const mysql = require('mysql');
 
 module.exports = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "reservacion_salas"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b65686317e6fe4",
+    password: "c8307c77",
+    database: "heroku_36d13502ba2c480"
 });
